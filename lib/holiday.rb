cd11765b41a736670fe0ab568627568c740c4afb
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 06:20:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,6 +46,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash[:season] => {
     holiday_name: supply_array
   }
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
