927508ebecdc1f5e2e246cb14d90f3d0d3bbe697
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 06:47:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season_string}:"
     holiday.each do |holiday, supply|
       holiday_string = holiday.to_s.capitalize
-      supply_string = supply.to_s
+      supply_string = supply.join(", ")
       puts "  #{holiday_string}: #{supply_string}"
     end
   end
