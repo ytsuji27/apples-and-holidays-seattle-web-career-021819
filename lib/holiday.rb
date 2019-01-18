212ce602302e9b8d062f54088c669647bcc56738
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 07:04:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
-  if holiday_hash.values.include?
+  if holiday_hash.values.include?("BBQ")
+    
   holiday_hash.keys
 end
 
